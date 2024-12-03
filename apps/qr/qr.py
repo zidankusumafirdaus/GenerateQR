49cde11c1136d7eea081945ba48c2b82a213797f
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2024-12-03 22:44:27 +0700

    QR in Folder Uploads

--- apps/qr/qr.py
+++ apps/qr/qr.py
@@ -1,8 +1,12 @@
 import os
 import qrcode
-from flask import Flask, request, render_template, Blueprint
+import hashlib
+from flask import Flask, request, render_template, Blueprint, send_from_directory
 
 qr = Blueprint("qr", __name__, static_folder="static", template_folder="templates")
+
+FolUploads = './apps/qr/uploads'
+os.makedirs(FolUploads, exist_ok=True)
 
 @qr.route('/barcode', methods=['GET', 'POST'])
 def barcode():
@@ -10,10 +14,14 @@ def barcode():
         link = request.form['link']
         QRGenerate = qrcode.make(link)
         
-        safe_link = link.replace('http://', '').replace('https://', '').replace('/', '')
-        qrSave = os.path.join('./apps/qr/static', f"{safe_link}.png")
+        safe_link = hashlib.md5(link.encode()).hexdigest() 
+        qrSave = os.path.join(FolUploads, f"{safe_link}.png")
 
         QRGenerate.save(qrSave)
         
         return render_template('qr.html', data=f"{safe_link}.png")
     return render_template('qr.html', data=None)
+
+@qr.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(FolUploads, filename)
